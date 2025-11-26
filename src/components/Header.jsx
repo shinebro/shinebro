5ae7a8fc36b04dfe1320894f1a1e22270f1b75eb
shinebro <shinebrofficial2@gmail.com>
2025-11-26 13:23:42 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -93,7 +93,7 @@ const Header = () => {
                                 {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
                             </button>
                             <Link to="/" className="flex items-center gap-2">
-                                <img src={logo} alt="ShineBro" className="h-25 md:w-25 object-contain" />
+                                <img src={logo} alt="ShineBro" className="h-22 md:w-22 object-contain" />
                             </Link>
                         </div>
 
@@ -199,6 +199,7 @@ const Header = () => {
 };
 
 export default Header;
+
 
 
 

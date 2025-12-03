15c9f955048fd4a7d2c591d39219b1bfbe9bd5fa
shinebro <shinebrofficial2@gmail.com>
2025-12-03 21:18:50 +0530

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -25,7 +25,7 @@ const Profile = () => {
         lastName: '',
         address: '',
         city: '',
-        pinCode: '',
+        zipCode: '',
         phone: ''
     });
     const [updateSuccess, setUpdateSuccess] = useState(false);

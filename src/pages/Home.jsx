7a965a272d443e35c8de6846590400f85db7d082
shinebro <shinebrofficial2@gmail.com>
2025-12-11 20:47:57 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -67,7 +67,7 @@ const Home = () => {
                     </div>
 
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-                        {products.map(product => (
+                        {products.filter(p => p.id !== 4).map(product => (
                             <ProductCard key={product.id} product={product} />
                         ))}
                     </div>

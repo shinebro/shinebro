b4b3a6c7ed8953edb080c7bc4959f0488c088b0e
shinebro <shinebrofficial2@gmail.com>
2025-12-20 20:42:45 +0530

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 
 const Layout = ({ children }) => {
+    const location = useLocation();
+    const isAdmin = location.pathname.startsWith('/admin');
+
     return (
         <div className="min-h-screen flex flex-col font-sans">
-            <Header />
+            {!isAdmin && <Header />}
             <main className="flex-grow">
                 {children}
             </main>
